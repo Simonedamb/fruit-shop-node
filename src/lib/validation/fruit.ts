a70eab8bbec34a4d1a7b26b01a020370eb5e7f9a
Simone D'ambrosio <simonedamb@hotmail.it>
2022-07-27 16:31:36 +0200

    schema-prisma

--- src/lib/validation/fruit.ts
+++ src/lib/validation/fruit.ts
@@ -5,7 +5,7 @@ export const fruitSchema = Type.Object(
         name: Type.String(),
         genus: Type.Optional(Type.String()),
         image: Type.String(),
-        price: Type.String(),
+        price: Type.Number(),
         family: Type.Optional(Type.String()),
         order: Type.String(),
     },
